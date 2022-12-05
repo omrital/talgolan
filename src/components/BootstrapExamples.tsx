49f22e0d0a742cbd125d48498a962aa994237a5b
omri tal <omrita@wix.com>
2022-12-05 21:02:38 +0200

    add titleBig component

--- src/components/BootstrapExamples.tsx
+++ src/components/BootstrapExamples.tsx
@@ -10,6 +10,169 @@ import logo from "../logo.svg";
 
 function BootstrapExamples() {
     const [show, setShow] = useState(false);
+    return (
+    <div className="Container">
+      {renderCategoriesSmallExample()}
+
+      {dddddddddddddddddddddddd()}
+
+      {renderCategoriesBigExample()}
+
+      {dddddddddddddddddddddddd()}
+
+      {Gallery()}
+
+      {dddddddddddddddddddddddd()}
+
+      {renderImageDisplay(false)}
+
+      {dddddddddddddddddddddddd()}
+
+      {ImageDisplayModal()}
+
+      {dddddddddddddddddddddddd()}
+
+      {titleBig()}
+
+      {dddddddddddddddddddddddd()}
+
+        <Row xs={2}>
+        <Col className="Col" sm>sm=true1</Col>
+        <Col className="Col" sm>sm=true2</Col>
+        <Col className="Col" sm>sm=true3</Col>
+        <Col className="Col" sm>sm=true4</Col>
+        <Col className="Col" sm>sm=true5</Col>
+        <Col className="Col" sm>sm=true6</Col>
+        <Col className="Col" sm>sm=true7</Col>
+        <Col className="Col" sm>sm=true8</Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      <Row>
+        <Col className="Col">1 of 2</Col>
+        <Col className="Col">2 of 2</Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      <Row>
+        <Col className="Col">1 of 3</Col>
+        <Col className="Col">2 of 3</Col>
+        <Col className="Col">3 of 3</Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      <Row>
+         <Col className="Col">1 of 3</Col>
+         <Col className="Col" xs={6}>2 of 3 (wider)</Col>
+         <Col className="Col">3 of 3</Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      <Row>
+         <Col className="Col">1 of 3</Col>
+         <Col className="Col" xs={5}>2 of 3 (wider)</Col>
+         <Col className="Col">3 of 3</Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      <Row className="justify-content-md-center">
+         <Col className="Col"  xs lg="2">
+          1 of 3
+        </Col>
+         <Col className="Col"  md="auto">Variable width content</Col>
+         <Col className="Col"  xs lg="2">
+          3 of 3
+        </Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      <Row>
+         <Col className="Col" >1 of 3</Col>
+         <Col className="Col"  md="auto">Variable width content</Col>
+         <Col className="Col"  xs lg="2">
+          3 of 3
+        </Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      <Row>
+         <Col className="Col"  sm={8}>sm=8</Col>
+         <Col className="Col"  sm={4}>sm=4</Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      {/* Stack the columns on mobile by making one full-width and the other half-width */}
+      <Row>
+         <Col className="Col"  xs={12} md={8}>
+          xs=12 md=8
+        </Col>
+         <Col className="Col"  xs={6} md={4}>
+          xs=6 md=4
+        </Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
+      <Row>
+         <Col className="Col"  xs={6} md={4}>
+          xs=6 md=4
+        </Col>
+         <Col className="Col"  xs={6} md={4}>
+          xs=6 md=4
+        </Col>
+         <Col className="Col"  xs={6} md={4}>
+          xs=6 md=4
+        </Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      {/* Columns are always 50% wide, on mobile and desktop */}
+      <Row>
+         <Col className="Col"  xs={6}>xs=6</Col>
+         <Col className="Col"  xs={6}>xs=6</Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      <Row>
+         <Col className="Col"  xs>First, but unordered</Col>
+         <Col className="Col"  xs={{ order: 12 }}>Second, but last</Col>
+         <Col className="Col"  xs={{ order: 1 }}>Third, but second</Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      <Row>
+         <Col className="Col"  xs={{ order: 'last' }}>First, but last</Col>
+         <Col className="Col"  xs>Second, but unordered</Col>
+         <Col className="Col"  xs={{ order: 'first' }}>Third, but first</Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      <Row>
+         <Col className="Col"  md={4}>md=4</Col>
+         <Col className="Col"  md={{ span: 4, offset: 4 }}>{`md={{ span: 4, offset: 4 }}`}</Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      <Row>
+         <Col className="Col"  md={{ span: 3, offset: 3 }}>{`md={{ span: 3, offset: 3 }}`}</Col>
+         <Col className="Col"  md={{ span: 3, offset: 3 }}>{`md={{ span: 3, offset: 3 }}`}</Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      <Row>
+         <Col className="Col"  md={{ span: 9, offset: 3 }}>{`md={{ span: 6, offset: 3 }}`}</Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      <Row xs={2} md={4} lg={6}>
+         <Col className="Col" >1 of 2</Col>
+         <Col className="Col" >2 of 2</Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      <Row xs={1} md={2}>
+         <Col className="Col" >1 of 3</Col>
+         <Col className="Col" >2 of 3</Col>
+         <Col className="Col" >3 of 3</Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      <Row xs="auto">
+         <Col className="Col" >1 of 3</Col>
+         <Col className="Col" >2 of 3</Col>
+         <Col className="Col" >3 of 3</Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+      <Row md={4}>
+         <Col className="Col" >1 of 3</Col>
+         <Col className="Col"  xs={6}>2 of 3</Col>
+         <Col className="Col" >3 of 3</Col>
+      </Row>
+      {dddddddddddddddddddddddd()}
+    </div>
+  );
 
     function ImageDisplayModal() {
         if (!show) {
@@ -49,162 +212,17 @@ function BootstrapExamples() {
             </Row>
         );
     }
+}
 
-    return (
-    <div className="Container">
-      {renderCategoriesSmallExample()}
-      {divider()}
-      {renderCategoriesBigExample()}
-      {divider()}
-      {Gallery()}
-      {divider()}
-      {renderImageDisplay(false)}
-      {divider()}
-      {/*{show && ImageDisplayModal1(show, setShow)}*/}
-      {ImageDisplayModal()}
-      {divider()}
-      <Row xs={2}>
-        <Col className="Col" sm>sm=true1</Col>
-        <Col className="Col" sm>sm=true2</Col>
-        <Col className="Col" sm>sm=true3</Col>
-        <Col className="Col" sm>sm=true4</Col>
-        <Col className="Col" sm>sm=true5</Col>
-        <Col className="Col" sm>sm=true6</Col>
-        <Col className="Col" sm>sm=true7</Col>
-        <Col className="Col" sm>sm=true8</Col>
-      </Row>
-      {divider()}
-      <Row>
-        <Col className="Col">1 of 2</Col>
-        <Col className="Col">2 of 2</Col>
-      </Row>
-      {divider()}
-      <Row>
-        <Col className="Col">1 of 3</Col>
-        <Col className="Col">2 of 3</Col>
-        <Col className="Col">3 of 3</Col>
-      </Row>
-      {divider()}
-      <Row>
-         <Col className="Col">1 of 3</Col>
-         <Col className="Col" xs={6}>2 of 3 (wider)</Col>
-         <Col className="Col">3 of 3</Col>
-      </Row>
-      {divider()}
-      <Row>
-         <Col className="Col">1 of 3</Col>
-         <Col className="Col" xs={5}>2 of 3 (wider)</Col>
-         <Col className="Col">3 of 3</Col>
-      </Row>
-      {divider()}
-      <Row className="justify-content-md-center">
-         <Col className="Col"  xs lg="2">
-          1 of 3
-        </Col>
-         <Col className="Col"  md="auto">Variable width content</Col>
-         <Col className="Col"  xs lg="2">
-          3 of 3
-        </Col>
-      </Row>
-      {divider()}
-      <Row>
-         <Col className="Col" >1 of 3</Col>
-         <Col className="Col"  md="auto">Variable width content</Col>
-         <Col className="Col"  xs lg="2">
-          3 of 3
-        </Col>
-      </Row>
-      {divider()}
-      <Row>
-         <Col className="Col"  sm={8}>sm=8</Col>
-         <Col className="Col"  sm={4}>sm=4</Col>
-      </Row>
-      {divider()}
-      {/* Stack the columns on mobile by making one full-width and the other half-width */}
-      <Row>
-         <Col className="Col"  xs={12} md={8}>
-          xs=12 md=8
-        </Col>
-         <Col className="Col"  xs={6} md={4}>
-          xs=6 md=4
-        </Col>
-      </Row>
-      {divider()}
-      {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
-      <Row>
-         <Col className="Col"  xs={6} md={4}>
-          xs=6 md=4
-        </Col>
-         <Col className="Col"  xs={6} md={4}>
-          xs=6 md=4
-        </Col>
-         <Col className="Col"  xs={6} md={4}>
-          xs=6 md=4
-        </Col>
-      </Row>
-      {divider()}
-      {/* Columns are always 50% wide, on mobile and desktop */}
-      <Row>
-         <Col className="Col"  xs={6}>xs=6</Col>
-         <Col className="Col"  xs={6}>xs=6</Col>
-      </Row>
-      {divider()}
-      <Row>
-         <Col className="Col"  xs>First, but unordered</Col>
-         <Col className="Col"  xs={{ order: 12 }}>Second, but last</Col>
-         <Col className="Col"  xs={{ order: 1 }}>Third, but second</Col>
-      </Row>
-      {divider()}
-      <Row>
-         <Col className="Col"  xs={{ order: 'last' }}>First, but last</Col>
-         <Col className="Col"  xs>Second, but unordered</Col>
-         <Col className="Col"  xs={{ order: 'first' }}>Third, but first</Col>
-      </Row>
-      {divider()}
-      <Row>
-         <Col className="Col"  md={4}>md=4</Col>
-         <Col className="Col"  md={{ span: 4, offset: 4 }}>{`md={{ span: 4, offset: 4 }}`}</Col>
-      </Row>
-      {divider()}
-      <Row>
-         <Col className="Col"  md={{ span: 3, offset: 3 }}>{`md={{ span: 3, offset: 3 }}`}</Col>
-         <Col className="Col"  md={{ span: 3, offset: 3 }}>{`md={{ span: 3, offset: 3 }}`}</Col>
-      </Row>
-      {divider()}
-      <Row>
-         <Col className="Col"  md={{ span: 9, offset: 3 }}>{`md={{ span: 6, offset: 3 }}`}</Col>
-      </Row>
-      {divider()}
-      <Row xs={2} md={4} lg={6}>
-         <Col className="Col" >1 of 2</Col>
-         <Col className="Col" >2 of 2</Col>
-      </Row>
-      {divider()}
-      <Row xs={1} md={2}>
-         <Col className="Col" >1 of 3</Col>
-         <Col className="Col" >2 of 3</Col>
-         <Col className="Col" >3 of 3</Col>
-      </Row>
-      {divider()}
-      <Row xs="auto">
-         <Col className="Col" >1 of 3</Col>
-         <Col className="Col" >2 of 3</Col>
-         <Col className="Col" >3 of 3</Col>
-      </Row>
-      {divider()}
-      <Row md={4}>
-         <Col className="Col" >1 of 3</Col>
-         <Col className="Col"  xs={6}>2 of 3</Col>
-         <Col className="Col" >3 of 3</Col>
-      </Row>
-      {divider()}
-    </div>
+function dddddddddddddddddddddddd() {
+  return (
+    <img src={logo} className="App-logo" alt="logo" />
   );
 }
 
-function divider() {
+function titleBig() {
   return (
-    <img src={logo} className="App-logo" alt="logo" />
+    <Col style={{backgroundImage: `url("big-title-background.png")`}} className="TitleBig" sm>Title Big</Col>
   );
 }
 
